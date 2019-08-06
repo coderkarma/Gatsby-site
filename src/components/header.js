33e480c0a1431a5d23f044a6e32dbe00376b31c6
karma <karmadrukpa11@gmail.com>
2019-08-05 18:50:00 -0700

    added custom header and content

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
+// import './header.module.scss';
+import headerStyles from './header.module.scss';
+
 const Header = () => {
 	return (
-		<header>
-			<h1>Karma Drukpa</h1>
+		<header className={headerStyles.header}>
+			<h1>
+				<Link className={headerStyles.title} to='/'>
+					Karma Drukpa
+				</Link>
+			</h1>
 			<nav>
 				<ul>
 					<li>
-						<Link to='/'>Home</Link>
+						<Link className={headerStyles.link} to='/'>
+							Home
+						</Link>
 					</li>
 					<li>
 						<Link to='/blog'>Blog</Link>
