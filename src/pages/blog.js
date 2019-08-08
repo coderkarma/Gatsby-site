7304da3ca4f70247190324ae5111df75c32db9e4
karma <karmadrukpa11@gmail.com>
2019-08-07 17:09:09 -0700

    showing post dynamically

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import Layout from '../components/layout';
+import { graphql, useStaticQuery } from 'gatsby';
 
 const BlogPage = () => {
+	const data = useStaticQuery(graphql`
+		query {
+			allMarkdownRemark {
+				edges {
+					node {
+						frontmatter {
+							title
+							date
+						}
+					}
+				}
+			}
+		}
+	`);
+
 	return (
 		<Layout>
 			<h1>Blog</h1>
-			<p>
-				Posts will show up here later <a href='wwww.google.com' />on{' '}
-			</p>
+			<ol>
+				{data.allMarkdownRemark.edges.map(ele => {
+					return (
+						<div>
+							<li>
+								<h2>{ele.node.frontmatter.title}</h2>
+								<p>{ele.node.frontmatter.date}</p>
+							</li>
+						</div>
+					);
+				})}
+			</ol>
 		</Layout>
 	);
 };
---
+++ src/posts/gatsby.md
@@ -0,0 +1,12 @@
+---
+title: "Gatsby Greatness"
+date: "2019-08-07"
+---
+
+Gatsby is great to play with if you want to build fast site!
+
+## Topics Covered
+
+1. Gatsby
+2. GraphQl
+3. React
